51233ce2f182f61bf161cd4919e22a9a0e0187ec
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-03-01 20:53:20 +0300

    1.5. Поля объекта [#460568]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -4,17 +4,19 @@ public class Cat {
 
     private String food;
 
+    private String name;
+
     public void show() {
         System.out.println(this.name + " " + this.food);
     }
 
     public void eat(String meat) {
+
         this.food = meat;
     }
 
-    private String name;
-
     public void giveNick(String nick) {
+
         this.name = nick;
     }
 
