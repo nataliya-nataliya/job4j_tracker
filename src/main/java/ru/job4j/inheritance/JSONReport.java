592f615e2b7e6ad93b88f8f349cd6794796a3bba
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-03-06 21:09:39 +0300

    4. Переопределение  [#460580]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,5 @@
 package ru.job4j.inheritance;
 
-
 public class JSONReport extends TextReport{
         public String generate(String name, String body) {
             return "name: " + name + System.lineSeparator() + "body: " + body;
