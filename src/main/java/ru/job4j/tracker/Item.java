929981cbc78ce1887ef0b35ecc162216f841b20d
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-03-14 13:54:36 +0300

    5. Tracker - хранилище [#460596]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,40 +1,30 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-
 public class Item {
-    private LocalDateTime created = LocalDateTime.now();
-
-    public LocalDateTime getCreated() {
-        return created;
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 
-//    private int id;
-//    private String name;
-//    public Item() {
-//    }
+    private int id;
+    private String name;
 
-//    public Item(int id) {
-//        this.id = id;
-//    }
-//
-//    public Item(int id, String name) {
-//        this.id = id;
-//        this.name = name;
-//    }
+    public int getId() {
+        return id;
+    }
 
-//   public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,25 +1,51 @@
-//package ru.job4j.tracker;
-//
-//public class Tracker {
-//    private final Item[] items = new Item[100];
-//    private int ids = 1;
-//    private int size = 0;
-//
-//    public Item add(Item item) {
-//        //item.setId(ids++);
-//        items[size++] = item;
-//        return item;
-//    }
-//
-//    public Item findById(int id) {
-//        Item rsl = null;
-//        for (int index = 0; index < size; index++) {
-//            Item item = items[index];
-//            //if (item.getId() == id) {
-//                rsl = item;
-        //        break;
-      //      }
-  //      }
-      //  return rsl;
-    //}
-//}
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class Tracker {
+
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+    public Item findByName(String key) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if(item.getName() == key) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+    public Item[] findAll() {
+        Item[] namesWithoutNull = new Item[items.length];
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (items[index] != null) {
+                namesWithoutNull[size] = items[index];
+                size++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+            return Arrays.copyOf(namesWithoutNull, size);
+    }
+
+    public Item findById(int id) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getId() == id) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
