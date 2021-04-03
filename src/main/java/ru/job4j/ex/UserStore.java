c0d596eee5f1ed4fe81970cf4b855b42ebb7249a
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-04-04 01:06:11 +0300

    0.5. Иерархия исключений и множественный catch. [#460539]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,37 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if ((user.getUsername()).equals(login)) {
+                return user;
+            } else {
+                throw new UserNotFoundException("User not found");
+            }
+        }
+        return null;
+    }
+
+        private static boolean validate (User user) throws UserInvalidException {
+            if (!user.isValid() || user.getUsername().length() < 3) {
+                throw new UserInvalidException("User is not valid");
+            }
+            return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
