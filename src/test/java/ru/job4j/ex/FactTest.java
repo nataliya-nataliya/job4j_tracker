948603ea945232568d6ffd890b9ebe6f6fbbcdf2
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-04-03 13:31:37 +0300

    0.3. Тестирование исключений с junit [#460537]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class FactTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void whenStartGreaterThenFinish() {
+    public void whenNumberLessThen0() {
         Fact.calc(-1);
     }
 }
