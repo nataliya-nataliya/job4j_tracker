9eb56c9be75d73379f9339900de0a11463c7de97
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-04-05 22:41:18 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#460540]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -5,9 +5,7 @@ public class FactRec {
         if (n == 0 || n == 1) {
             return 1;
         }
-        else {
             return calc(n - 1) * n;
-        }
     }
 
     public static void main(String[] args) {
