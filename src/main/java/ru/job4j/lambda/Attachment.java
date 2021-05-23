e7945938e0c6f90cc1287a7219529bbf44e49c3e
Nataliya-nataliya <nataliya.chest@gmail.com>
2021-05-23 22:24:01 +0300

    1.1. Анонимные классы [#214142]

---
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -0,0 +1,34 @@
+package ru.job4j.lambda;
+
+/**
+ * Класс содержит поля имени и размера, геттеры, метод toString
+ * @author Nataliya
+ * @version 1.0
+ */
+
+public class Attachment {
+
+    private String name;
+    private int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "Attachment{"
+                + "name='" + name + '\''
+                + ", size=" + size
+                + '}';
+    }
+}
